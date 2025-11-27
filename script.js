14acfa00c3c9af28985a952437ff01087cb2b2b2
Ashlyn Knox <ashx3s@tuta.io>
2025-11-27 14:49:27 -0700

    added interactive click event version

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // 1. Basic If Else Pattern with DOM Manipulation
 
-let timeOfDay = "Evening";
+let timeOfDay = "morning";
 const greetElOne = document.querySelector("#greeting-message-field");
 
 if (timeOfDay.toLowerCase() === "morning") {
@@ -15,13 +15,43 @@ if (timeOfDay.toLowerCase() === "morning") {
   greetElOne.textContent = "Time of day is unknown";
 }
 
-// Create time of day variable that could be for the afternoon, morning, evening
-
 // 2. Advanced Pattern Object Lookup
 // greetings object can be used in the first example
-
+const greetings = {
+  morning: "Good Morning!!",
+  afternoon: "Good Afternoon!!",
+  evening: "Good Evening!!",
+};
+const greetElTwo = document.querySelector("#greeting-field");
 // function getGreeting.
+const getGreeting = (time) =>
+  greetings[time.toLowerCase()] || "The time of day is unknown";
+
+greetElTwo.textContent = getGreeting(timeOfDay);
 
 // Example 3: Interactive array based cycling
 
-// TODO: User Logged in Toggle
+const greetArr = [
+  "Glorious Morning!!!",
+  "Fabulous Afternoon!!!",
+  "Stupendous Evening!!!",
+];
+
+let currentIndex = 0;
+
+// function declaration
+const updateGreeting = () => {
+  const greetField = document.querySelector("#greeting-field-interactive");
+  greetField.textContent = greetArr[currentIndex];
+};
+
+// function invocation
+updateGreeting();
+
+const toggleButton = document.querySelector("#cycle-time-button");
+
+toggleButton.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % greetArr.length;
+  console.log(currentIndex);
+  updateGreeting();
+});
