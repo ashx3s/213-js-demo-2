890858614de3a5c85bed384d1a630b402d8555da
Ashlyn Knox <ashx3s@tuta.io>
2025-11-27 14:33:45 -0700

    added conditional logic for time of day greeter ex

--- script.js
+++ script.js
@@ -2,6 +2,19 @@
 
 // 1. Basic If Else Pattern with DOM Manipulation
 
+let timeOfDay = "Evening";
+const greetElOne = document.querySelector("#greeting-message-field");
+
+if (timeOfDay.toLowerCase() === "morning") {
+  greetElOne.textContent = "Good Morning";
+} else if (timeOfDay.toLowerCase() === "afternoon") {
+  greetElOne.textContent = "Good Afternoon";
+} else if (timeOfDay.toLowerCase() === "evening") {
+  greetElOne.textContent = "Good Evening";
+} else {
+  greetElOne.textContent = "Time of day is unknown";
+}
+
 // Create time of day variable that could be for the afternoon, morning, evening
 
 // 2. Advanced Pattern Object Lookup
